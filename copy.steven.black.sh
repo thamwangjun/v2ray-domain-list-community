e158c4981b1d54cbb066e2557a9be55918b0dc7f
Tham Wang Jun <6513021+thamwangjun@users.noreply.github.com>
2023-03-05 11:46:00 +0800

    add steven black

---
+++ copy.steven.black.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+cp hosts/steven.black.base data/steven.black.base
+cp hosts/steven.black.fakenews data/steven.black.fakenews
+cp hosts/steven.black.gambling data/steven.black.gambling
+cp hosts/steven.black.porn data/steven.black.porn
+cp hosts/steven.black.social data/steven.black.social
